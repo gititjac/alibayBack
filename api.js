7b4c9c229e2e464a031b72f8cda431104c1079a9
Your Name <you@example.com>
2018-08-06 09:34:30 -0400

    second

--- api.js
+++ api.js
@@ -7,4 +7,31 @@ app.get('/itemsBought', (req, res) => {
     res.send(JSON.stringify(alibay.getItemsBought(uid)));
 });
 
+app.post('/signup', (req, res) =>{
+    let body = req.body.toString();
+    let parsed = JSON.parse(body);
+    let username = parsed.username;
+    let password = parsed.password;
+    
+    if (username.length > 0 && password.length > 5) {
+        res.send('your userid is: ' + (Math.ceil(Math.random()*100000))) //generate userid here?
+    }
+    else {
+        res.send('password too short')
+    }
+})
+
+app.post('/login',(req,res) => {
+    let body = req.body.toString();
+    let parsed = JSON.parse(body);
+    let username = parsed.username;
+    let password = parsed.password;
+    if (username === "bob" && password === "pw123") {
+        res.send('login successful')
+    }
+    else {
+        res.send('invalid username or password')
+    }
+})
+
 app.listen(3000, () => console.log('Listening on port 3000!'))
